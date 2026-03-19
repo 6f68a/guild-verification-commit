cf237e4cb814f8271890cf7e996e1e4270132093
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:59:01 +0700

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:58:17.005Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-19T15:59:00.158Z
+Commit: Fix broken links in documentation
