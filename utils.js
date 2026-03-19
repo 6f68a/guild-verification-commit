a6726ef2e511aab7647ff1bebd75e0b86246e6df
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:53:34 +0700

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:52:46.359Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-19T15:53:33.606Z
+Commit: Resolve null pointer exception in user service
