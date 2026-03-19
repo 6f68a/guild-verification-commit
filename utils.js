0d8cc2d5d7acf7c24d8b7b1ce25b11a0456282c2
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:52:12 +0700

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:51:59.687Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-19T15:52:11.369Z
+Commit: Fix broken links in documentation
