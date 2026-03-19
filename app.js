cbe7efe077b0055f73438981f72daf948e0b70a3
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:58:20 +0700

    Add multi-language support

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:57:58.796Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-19T15:58:19.158Z
+Commit: Add multi-language support
