50f8ffcfc0e12cfec36a4c35a5fe175ce9e93138
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:57:24 +0700

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:55:55.961Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-19T15:57:22.960Z
+Commit: Fix memory leak in event handlers
