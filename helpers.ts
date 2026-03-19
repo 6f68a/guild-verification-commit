76397727f401a77d5376700d976c3b53c251673a
6f68a <phuquocisphuquoc@gmail.com>
2026-03-19 22:58:26 +0700

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-19T15:58:08.279Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-19T15:58:25.705Z
+Commit: Add dark mode support
